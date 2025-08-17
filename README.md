00ac814018649c061bcd267c615873712eef0bf9
Duncan McGreggor <oubiwann@gmail.com>
2025-08-17 03:43:33 -0500

    Updated docs, etc.

--- README.md
+++ README.md
@@ -1,41 +1,50 @@
 # colours
 
-*An ANSI colour library for Clojure*
+*Another ANSI colour library for Clojure*
 
 ## Installation
 
-TBD
+`deps.edn`:
 
-## Usage
+```clojure
+{:deps {com.github.clojusc/colours {:mvn/version "0.1.0"}}}
+```
 
-Run the project directly, via `:exec-fn`:
+A lein `project.clj` file:
+
+
+```clojure
+(defproject my-project "1.0.0"
+  :dependencies [[com.github.clojusc/colours "0.1.0"]])
+```
+
+## Demo
+
+Run the project's demo directly, via `:exec-fn`:
 
     $ clojure -X:run-x
     Hello, Clojure!
 
-Run the project's tests (they'll fail until you edit them):
+## Usage
 
-    $ clojure -T:build test
+The demo runs example code like the following:
 
-Run the project's CI pipeline and build an uberjar (this will fail until you edit the tests to pass):
+```clojure
+  (colours/print (colours/colour attr/fg-green attr/bold) "Hello, ")
+  (colours/print (colours/colour attr/fg-cyan) "World")
+  (colours/println (colours/colour attr/fg-green attr/bold) "!\n")
 
-    $ clojure -T:build ci
+  (colours/red "This is red text\n")
+  (colours/green "This is green text\n")
+  (colours/blue "This is blue text\n")
+  (colours/println (colours/colour attr/fg-yellow attr/bold) "Bold yellow text")
 
-## Options
+  (let [orange (colours/rgb 255 128 0)]
+    (colours/println orange "This is orange RGB text"))
 
-FIXME: listing of options this app accepts.
-
-## Examples
-
-...
-
-### Bugs
-
-...
-
-### Any Other Sections
-### That You Think
-### Might be Useful
+  (println "\nString formatting:")
+  (println "Status:" (colours/green-string "SUCCESS") "- Don't worry, be happy!\n"))
+```
 
 ## License
 
--- src/clojusc/colours.clj
+++ src/clojusc/colours.clj
@@ -3,27 +3,26 @@
             [clojusc.colours.attr :as attr])
   (:gen-class))
 
-(defn greet
-  "Colourful greeting function demonstrating the colour library."
-  [data]
+(defn demo
+  "A quick demo."
+  [_]
   (println "\nColour library demo ...\n")
-  (let [name (or (:name data) "World")]
-    (colours/print (colours/colour attr/fg-green attr/bold) "Hello, ")
-    (colours/print (colours/colour attr/fg-cyan) name)
-    (colours/println (colours/colour attr/fg-green attr/bold) "!\n")
+  (colours/print (colours/colour attr/fg-green attr/bold) "Hello, ")
+  (colours/print (colours/colour attr/fg-cyan) "World")
+  (colours/println (colours/colour attr/fg-green attr/bold) "!\n")
 
-    (colours/red "This is red text\n")
-    (colours/green "This is green text\n")
-    (colours/blue "This is blue text\n")
-    (colours/println (colours/colour attr/fg-yellow attr/bold) "Bold yellow text")
-    
-    (let [orange (colours/rgb 255 128 0)]
-      (colours/println orange "This is orange RGB text"))
-    
-    (println "\nString formatting:")
-    (println "Status:" (colours/green-string "SUCCESS") "- Don't worry, be happy!\n")))
+  (colours/red "This is red text\n")
+  (colours/green "This is green text\n")
+  (colours/blue "This is blue text\n")
+  (colours/println (colours/colour attr/fg-yellow attr/bold) "Bold yellow text")
+
+  (let [orange (colours/rgb 255 128 0)]
+    (colours/println orange "This is orange RGB text"))
+
+  (println "\nString formatting:")
+  (println "Status:" (colours/green-string "SUCCESS") "- Don't worry, be happy!\n"))
 
 (defn -main
   "Colour library demonstration."
-  [& args]
-  (greet {:name (first args)}))
+  [& _]
+  (demo))
